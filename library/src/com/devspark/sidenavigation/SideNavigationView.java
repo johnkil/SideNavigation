0753296dbcdc67d08ab7924cfc7b1351176f6407
Evgeny Shishkin <johnkil78@gmail.com>
2013-01-07 22:59:18 +0400

    #6 Support menu items without icons

--- library/src/com/devspark/sidenavigation/SideNavigationView.java
+++ library/src/com/devspark/sidenavigation/SideNavigationView.java
@@ -27,230 +27,249 @@ import android.widget.TextView;
  * 
  */
 public class SideNavigationView extends LinearLayout {
-	private static final String LOG_TAG = SideNavigationView.class.getSimpleName();
+    private static final String LOG_TAG = SideNavigationView.class.getSimpleName();
 
-	private LinearLayout navigationMenu;
-	private ListView listView;
-	private View outsideView;
+    private LinearLayout navigationMenu;
+    private ListView listView;
+    private View outsideView;
 
-	private ISideNavigationCallback callback;
-	private ArrayList<SideNavigationItem> menuItems;
+    private ISideNavigationCallback callback;
+    private ArrayList<SideNavigationItem> menuItems;
 
-	/**
-	 * Constructor of {@link SideNavigationView}.
-	 * @param context
-	 */
-	public SideNavigationView(Context context) {
-		super(context);
-		load();
-	}
+    /**
+     * Constructor of {@link SideNavigationView}.
+     * 
+     * @param context
+     */
+    public SideNavigationView(Context context) {
+        super(context);
+        load();
+    }
 
-	/**
-	 * Constructor of {@link SideNavigationView}.
-	 * @param context
-	 * @param attrs
-	 */
-	public SideNavigationView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		load();
-	}
+    /**
+     * Constructor of {@link SideNavigationView}.
+     * 
+     * @param context
+     * @param attrs
+     */
+    public SideNavigationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        load();
+    }
 
-	/**
-	 * Loading of side navigation view.
-	 */
-	private void load() {
-		if (isInEditMode()) {
-			return;
-		}
-		initView();
-	}
+    /**
+     * Loading of side navigation view.
+     */
+    private void load() {
+        if (isInEditMode()) {
+            return;
+        }
+        initView();
+    }
 
-	/**
-	 * Initialization layout of side menu.
-	 */
-	private void initView() {
-		LayoutInflater.from(getContext()).inflate(R.layout.side_navigation, this, true);
-		navigationMenu = (LinearLayout) findViewById(R.id.side_navigation_menu);
-		listView = (ListView) findViewById(R.id.side_navigation_listview);
-		outsideView = (View) findViewById(R.id.side_navigation_outside_view);
-		outsideView.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				hideMenu();
-			}
-		});
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				if (callback != null) {
-					callback.onSideNavigationItemClick(menuItems.get(position).getId());
-				}
-				hideMenu();
-			}
-		});
-	}
+    /**
+     * Initialization layout of side menu.
+     */
+    private void initView() {
+        LayoutInflater.from(getContext()).inflate(R.layout.side_navigation, this, true);
+        navigationMenu = (LinearLayout) findViewById(R.id.side_navigation_menu);
+        listView = (ListView) findViewById(R.id.side_navigation_listview);
+        outsideView = (View) findViewById(R.id.side_navigation_outside_view);
+        outsideView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideMenu();
+            }
+        });
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (callback != null) {
+                    callback.onSideNavigationItemClick(menuItems.get(position).getId());
+                }
+                hideMenu();
+            }
+        });
+    }
 
-	/**
-	 * Setup of {@link ISideNavigationCallback} for callback of item click.
-	 * @param callback
-	 */
-	public void setMenuClickCallback(ISideNavigationCallback callback) {
-		this.callback = callback;
-	}
+    /**
+     * Setup of {@link ISideNavigationCallback} for callback of item click.
+     * 
+     * @param callback
+     */
+    public void setMenuClickCallback(ISideNavigationCallback callback) {
+        this.callback = callback;
+    }
 
-	/**
-	 * Setup of side menu items.
-	 * @param menu - resource ID
-	 */
-	public void setMenuItems(int menu) {
-		parseXml(menu);
-		if (menuItems != null && menuItems.size() > 0) {
-			listView.setAdapter(new SideNavigationAdapter());
-		}
-	}
+    /**
+     * Setup of side menu items.
+     * 
+     * @param menu - resource ID
+     */
+    public void setMenuItems(int menu) {
+        parseXml(menu);
+        if (menuItems != null && menuItems.size() > 0) {
+            listView.setAdapter(new SideNavigationAdapter());
+        }
+    }
 
-	/**
+    /**
 	 * 
 	 */
-	public void setBackgroundResource(int resource) {
-		listView.setBackgroundResource(resource);
-	}
+    public void setBackgroundResource(int resource) {
+        listView.setBackgroundResource(resource);
+    }
 
-	/**
-	 * Show side navigation menu.
-	 */
-	public void showMenu() {
-		outsideView.setVisibility(View.VISIBLE);
-		outsideView.startAnimation(AnimationUtils.loadAnimation(getContext(),
-				R.anim.side_navigation_fade_in));
-		navigationMenu.setVisibility(View.VISIBLE);
-		navigationMenu.startAnimation(AnimationUtils.loadAnimation(getContext(),
-				R.anim.side_navigation_in_from_left));
-	}
+    /**
+     * Show side navigation menu.
+     */
+    public void showMenu() {
+        outsideView.setVisibility(View.VISIBLE);
+        outsideView.startAnimation(AnimationUtils.loadAnimation(getContext(),
+                R.anim.side_navigation_fade_in));
+        navigationMenu.setVisibility(View.VISIBLE);
+        navigationMenu.startAnimation(AnimationUtils.loadAnimation(getContext(),
+                R.anim.side_navigation_in_from_left));
+    }
 
-	/**
-	 * Hide side navigation menu.
-	 */
-	public void hideMenu() {
-		outsideView.setVisibility(View.GONE);
-		outsideView.startAnimation(AnimationUtils.loadAnimation(getContext(),
-				R.anim.side_navigation_fade_out));
-		navigationMenu.setVisibility(View.GONE);
-		navigationMenu.startAnimation(AnimationUtils.loadAnimation(getContext(),
-				R.anim.side_navigation_out_to_left));
-	}
+    /**
+     * Hide side navigation menu.
+     */
+    public void hideMenu() {
+        outsideView.setVisibility(View.GONE);
+        outsideView.startAnimation(AnimationUtils.loadAnimation(getContext(),
+                R.anim.side_navigation_fade_out));
+        navigationMenu.setVisibility(View.GONE);
+        navigationMenu.startAnimation(AnimationUtils.loadAnimation(getContext(),
+                R.anim.side_navigation_out_to_left));
+    }
 
-	/**
-	 * Show/Hide side navigation menu depending on visibility.
-	 */
-	public void toggleMenu() {
-		if (isShown()) {
-			hideMenu();
-		} else {
-			showMenu();
-		}
-	}
-	
-	@Override
-	public boolean isShown() {
-		return navigationMenu.isShown();
-	}
+    /**
+     * Show/Hide side navigation menu depending on visibility.
+     */
+    public void toggleMenu() {
+        if (isShown()) {
+            hideMenu();
+        } else {
+            showMenu();
+        }
+    }
 
-	/**
-	 * Parse XML describe menu.
-	 * @param menu - resource ID
-	 */
-	private void parseXml(int menu) {
-		menuItems = new ArrayList<SideNavigationItem>();
-		try {
-			XmlResourceParser xrp = getResources().getXml(menu);
-			xrp.next();
-			int eventType = xrp.getEventType();
-			while (eventType != XmlPullParser.END_DOCUMENT) {
-				if (eventType == XmlPullParser.START_TAG) {
-					String elemName = xrp.getName();
-					if (elemName.equals("item")) {
-						String textId = xrp.getAttributeValue(
-								"http://schemas.android.com/apk/res/android",
-								"title");
-						String iconId = xrp.getAttributeValue(
-								"http://schemas.android.com/apk/res/android",
-								"icon");
-						String resId = xrp.getAttributeValue(
-								"http://schemas.android.com/apk/res/android",
-								"id");
-						SideNavigationItem item = new SideNavigationItem();
-						item.setId(Integer.valueOf(resId.replace("@", "")));
-						item.setText(resourceIdToString(textId));
-						item.setIcon(Integer.valueOf(iconId.replace("@", "")));
-						menuItems.add(item);
-					}
-				}
-				eventType = xrp.next();
-			}
-		} catch (Exception e) {
-			Log.w(LOG_TAG, e);
-		}
-	}
+    @Override
+    public boolean isShown() {
+        return navigationMenu.isShown();
+    }
 
-	/**
-	 * Convert resource ID to String.
-	 * @param text
-	 * @return
-	 */
-	private String resourceIdToString(String resId) {
-		if (!resId.contains("@")) {
-			return resId;
-		} else {
-			String id = resId.replace("@", "");
-			return getResources().getString(Integer.valueOf(id));
-		}
-	}
+    /**
+     * Parse XML describe menu.
+     * 
+     * @param menu - resource ID
+     */
+    private void parseXml(int menu) {
+        menuItems = new ArrayList<SideNavigationItem>();
+        try {
+            XmlResourceParser xrp = getResources().getXml(menu);
+            xrp.next();
+            int eventType = xrp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    String elemName = xrp.getName();
+                    if (elemName.equals("item")) {
+                        String textId = xrp.getAttributeValue(
+                                "http://schemas.android.com/apk/res/android",
+                                "title");
+                        String iconId = xrp.getAttributeValue(
+                                "http://schemas.android.com/apk/res/android",
+                                "icon");
+                        String resId = xrp.getAttributeValue(
+                                "http://schemas.android.com/apk/res/android",
+                                "id");
+                        SideNavigationItem item = new SideNavigationItem();
+                        item.setId(Integer.valueOf(resId.replace("@", "")));
+                        item.setText(resourceIdToString(textId));
+                        if (iconId != null) {
+                            try {
+                                item.setIcon(Integer.valueOf(iconId.replace("@", "")));
+                            } catch (NumberFormatException e) {
+                                Log.d(LOG_TAG, "Item " + item.getId() + " not have icon");
+                            }
+                        }
+                        menuItems.add(item);
+                    }
+                }
+                eventType = xrp.next();
+            }
+        } catch (Exception e) {
+            Log.w(LOG_TAG, e);
+        }
+    }
 
-	private class SideNavigationAdapter extends BaseAdapter {
-		private LayoutInflater inflater;
+    /**
+     * Convert resource ID to String.
+     * 
+     * @param text
+     * @return
+     */
+    private String resourceIdToString(String resId) {
+        if (!resId.contains("@")) {
+            return resId;
+        } else {
+            String id = resId.replace("@", "");
+            return getResources().getString(Integer.valueOf(id));
+        }
+    }
 
-		public SideNavigationAdapter() {
-			inflater = LayoutInflater.from(getContext());
-		}
+    private class SideNavigationAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
 
-		@Override
-		public int getCount() {
-			return menuItems.size();
-		}
+        public SideNavigationAdapter() {
+            inflater = LayoutInflater.from(getContext());
+        }
 
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
+        @Override
+        public int getCount() {
+            return menuItems.size();
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			final ViewHolder holder;
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.side_navigation_item, null);
-				holder = new ViewHolder();
-				holder.image = (ImageView) convertView.findViewById(R.id.side_navigation_item_icon);
-				holder.text = (TextView) convertView.findViewById(R.id.side_navigation_item_text);
-				convertView.setTag(holder);
-			} else {
-				holder = (ViewHolder) convertView.getTag();
-			}
-			holder.image.setImageResource(menuItems.get(position).getIcon());
-			holder.text.setText(menuItems.get(position).getText());
-			return convertView;
-		}
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
 
-		class ViewHolder {
-			TextView text;
-			ImageView image;
-		}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.side_navigation_item, null);
+                holder = new ViewHolder();
+                holder.text = (TextView) convertView.findViewById(R.id.side_navigation_item_text);
+                holder.icon = (ImageView) convertView.findViewById(R.id.side_navigation_item_icon);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            
+            SideNavigationItem item = menuItems.get(position);
+            holder.text.setText(menuItems.get(position).getText());
+            if (item.getIcon() != SideNavigationItem.DEFAULT_ICON_VALUE) {
+                holder.icon.setVisibility(View.VISIBLE);
+                holder.icon.setImageResource(menuItems.get(position).getIcon());
+            } else {
+                holder.icon.setVisibility(View.GONE);
+            }
+            return convertView;
+        }
 
-	}
+        class ViewHolder {
+            TextView text;
+            ImageView icon;
+        }
+
+    }
 
 }
