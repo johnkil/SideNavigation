665674619dc11a5168a42b2b46d7b2c2639b2141
Evgeny Shishkin <johnkil78@gmail.com>
2013-01-23 00:43:31 +0400

    #7 Support open the menu on the right.

--- sample/src/com/devspark/sidenavigation/sample/MainActivity.java
+++ sample/src/com/devspark/sidenavigation/sample/MainActivity.java
@@ -1,17 +1,8 @@
 /*
- * Copyright (C) 2012 Evgeny Shishkin
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2012 Evgeny Shishkin Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
  */
 
 package com.devspark.sidenavigation.sample;
@@ -21,20 +12,22 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.devspark.sidenavigation.ISideNavigationCallback;
 import com.devspark.sidenavigation.SideNavigationView;
+import com.devspark.sidenavigation.SideNavigationView.Mode;
 
 /**
  * 
  * @author e.shishkin
  * 
  */
-public class MainActivity extends SherlockActivity implements
-        ISideNavigationCallback {
+public class MainActivity extends SherlockActivity implements ISideNavigationCallback {
 
     public static final String EXTRA_TITLE = "com.devspark.sidenavigation.sample.extra.MTGOBJECT";
     public static final String EXTRA_RESOURCE_ID = "com.devspark.sidenavigation.sample.extra.RESOURCE_ID";
+    public static final String EXTRA_MODE = "com.devspark.sidenavigation.sample.extra.MODE";
 
     private ImageView icon;
     private SideNavigationView sideNavigationView;
@@ -54,9 +47,21 @@ public class MainActivity extends SherlockActivity implements
             int resId = getIntent().getIntExtra(EXTRA_RESOURCE_ID, 0);
             setTitle(title);
             icon.setImageResource(resId);
+            sideNavigationView.setMode(getIntent().getIntExtra(EXTRA_MODE, 0) == 0 ? Mode.LEFT : Mode.RIGHT);
         }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.main_menu, menu);
+        if (sideNavigationView.getMode() == Mode.RIGHT) {
+            menu.findItem(R.id.mode_right).setChecked(true);
+        } else {
+            menu.findItem(R.id.mode_left).setChecked(true);
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -65,6 +70,15 @@ public class MainActivity extends SherlockActivity implements
             case android.R.id.home:
                 sideNavigationView.toggleMenu();
                 break;
+            case R.id.mode_left:
+                item.setChecked(true);
+                sideNavigationView.setMode(Mode.LEFT);
+                break;
+            case R.id.mode_right:
+                item.setChecked(true);
+                sideNavigationView.setMode(Mode.RIGHT);
+                break;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -120,6 +134,7 @@ public class MainActivity extends SherlockActivity implements
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra(EXTRA_TITLE, title);
         intent.putExtra(EXTRA_RESOURCE_ID, resId);
+        intent.putExtra(EXTRA_MODE, sideNavigationView.getMode() == Mode.LEFT ? 0 : 1);
 
         // all of the other activities on top of it will be closed and this
         // Intent will be delivered to the (now on top) old activity as a
